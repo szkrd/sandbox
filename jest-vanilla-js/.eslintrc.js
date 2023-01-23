ebbe6db8f91cc545bf7782385d7450825d91eb2f
Szabolcs Kurdi <szabolcs.kurdi@gmail.com>
2023-01-23 16:42:05 +0100

    jest for vanilla js

---
+++ jest-vanilla-js/.eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {},
+};
